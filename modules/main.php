404dec67b8a4e0d1760ff03cffd448e6ec1a608e
Hoang Danh Thuong <thuonghoang.bhtech@gmail.com>
2018-05-08 22:41:37 +0700

    ddd

--- modules/main.php
+++ modules/main.php
@@ -1,8 +1,9 @@
 <?php
 include("modules/content/trang_chu.php");
+
+include('modules/content/search.php');
 if(!isset($_SESSION['dang_nhap_dien_dan'])){
 	include('modules/content/dang_ky.php');
 	include('modules/content/dang_nhap.php');	
-	include('modules/content/search.php');
 }
 ?>
